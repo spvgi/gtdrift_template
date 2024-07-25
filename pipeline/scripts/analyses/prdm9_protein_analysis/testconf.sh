7db4ab48613fa7a606fbb219f2b5d79740654438
Simon Penel <simon.penel@univ-lyon1.fr>
2024-07-25 13:19:52 +0200

    Add testconf.sh

---
+++ pipeline/scripts/analyses/prdm9_protein_analysis/testconf.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/sh
+# Test if softwraes are available
+
+# testing test
+
+which programe_bidon
+status=$?
+echo "Test bidon : $status"
+if  [ $status -eq 1 ]
+then
+echo "Ok"
+else
+echo "Error."
+exit
+fi
+
+
+
+echo "MISSING SOFTWARE " > missing_soft 
+echo "================ " >> missing_soft 
+
+
+
+# Executables
+# ===========
+
+for soft in "guix"  "formatdb" "blastp" "python3" "pip3" "bidon"
+do
+
+which $soft
+status=$?
+echo "Test $soft : $status"
+if  [ $status -eq 0 ]
+then
+echo "Ok"
+else
+echo "Error, $soft is missing"
+echo $soft >> missing_soft 
+fi
+done
+
+
+# Module phython
+# ==============
+
+which pip3
+status=$?
+if  [ $status -eq 0 ]
+then
+echo "pip3 Ok"
+else
+echo "Error, pip3 is missing"
+exit
+fi
+
+
+echo "" >> missing_soft 
+echo "MISSING MODULES " >> missing_soft 
+echo "=============== " >> missing_soft 
+
+for module in  "pandas" "bidon_mod" "ete3"
+do
+echo test module $module
+pip3 list|grep "^$module "
+status=$?
+echo "Module $module : $status"
+if  [ $status -eq 0 ]
+then
+echo "Ok"
+else
+echo "Error, module $module is missing"
+echo $module >> missing_soft 
+fi
+done
+
+
+
+
+
+
+ 
+
+
