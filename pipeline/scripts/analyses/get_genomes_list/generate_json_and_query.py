3bfa4755e65ba687199d2111baf180af6093cf86
Simon Penel <simon.penel@univ-lyon1.fr>
2024-10-18 09:39:57 +0200

    Check for GCA/GCF redundancy in generate_json_and_query.py

--- pipeline/scripts/analyses/get_genomes_list/generate_json_and_query.py
+++ pipeline/scripts/analyses/get_genomes_list/generate_json_and_query.py
@@ -1,13 +1,44 @@
 import sys
 
 # This script is used to generate the json and query used in json files of other scripts
+# python3 generate_json_and_query.py data/resources/organisms_list conf_assembly_list conf_query 
 
-with open(sys.argv[1]) as reader, open(sys.argv[2], 'w') as writer, open(sys.argv[3], 'w') as writer2:
+dico = {}
+with open(sys.argv[1]) as reader:
     data = reader.readlines()
+    for line in data[1:]:
+        acc = line.split(".")[0][4:]
+        if acc in dico:
+            dico[acc].append(line.rstrip())
+        else :
+            dico[acc] = [line.rstrip()]
+unic_dico = {}
+for key in dico:
+    val  = dico[key]
+    if len(val) == 1 :
+        unic_dico[key] = val[0]
+    if len(val) == 2 :
+        test = 0
+        for candidat in val:
+            if candidat[0:3] == "GCF" :
+                unic_dico[key] = candidat
+                test += 1
+        if test != 1 :
+            sys.exit("we want one and only one  GCF")
+    if len(val) > 2 :
+        sys.exit("too many assemblies")
+accessions = list(unic_dico.values())
+with open(sys.argv[2], 'w') as writer, open(sys.argv[2]+".col", 'w') as writer_col, open(sys.argv[3], 'w') as writer2, open(sys.argv[3]+".col", 'w') as writer2_col:
     writer.write("[ ")
-    writer.write('"'+data[1].rstrip()+'" ')
-    writer2.write('('+data[1].split(".")[0]+') ')    
-    for line in data[2:]:
-            writer.write(', "'+line.rstrip()+'" ')
+    writer.write('"'+accessions[0]+'" ')
+    writer_col.write("[ ")
+    writer_col.write('"'+accessions[0]+'" ')
+    writer2.write('('+accessions[0].split(".")[0]+') ')
+    writer2_col.write('('+accessions[0].split(".")[0]+') ')
+    for line in accessions[1:]:
+            writer.write(', "'+line+'" ')
+            writer_col.write(', \n"'+line+'" ')
             writer2.write('OR ('+line.split(".")[0]+') ')
+            writer2_col.write('\n OR ('+line.split(".")[0]+') ')
     writer.write("]")
+    writer_col.write("]")
